a371e0745f743d90dc6281ca5b448a9a012ee957
naveen03252 <naveen.chitturi@bizcloudexperts.com>
2024-07-30 17:28:08 +0530

    removing the table

--- ssm.tf
+++ ssm.tf
@@ -12,31 +12,31 @@ resource "aws_ssm_parameter" "ltl_location_update_queue_arn" {
   }
 }
 
-resource "aws_ssm_parameter" "omni_ltl_location_update_log_table_name" {
-  name  = "/${var.application}/${var.env}/ltl-location-update-log-table-name"
-  type  = "String"
-  value = aws_dynamodb_table.omni_ltl_location_update_log_table.name
-  tags = {
-    Name  = "/${var.application}/${var.env}/ltl-location-update-log-table-name"
-    Application = var.application
-    CreatedBy   = var.created_by
-    Environment = var.env
-    STAGE       = var.env
-  }
-}
+# resource "aws_ssm_parameter" "omni_ltl_location_update_log_table_name" {
+#   name  = "/${var.application}/${var.env}/ltl-location-update-log-table-name"
+#   type  = "String"
+#   value = aws_dynamodb_table.omni_ltl_location_update_log_table.name
+#   tags = {
+#     Name  = "/${var.application}/${var.env}/ltl-location-update-log-table-name"
+#     Application = var.application
+#     CreatedBy   = var.created_by
+#     Environment = var.env
+#     STAGE       = var.env
+#   }
+# }
 
-resource "aws_ssm_parameter" "omni_ltl_location_update_log_table_status_index" {
-  name  = "/${var.application}/${var.env}/ltl-location-update-log-table-status-index"
-  type  = "String"
-  value = "Status-index"
-  tags = {
-    Name  = "/${var.application}/${var.env}/ltl-location-update-log-table-status-index"
-    Application = var.application
-    CreatedBy   = var.created_by
-    Environment = var.env
-    STAGE       = var.env
-  }
-}
+# resource "aws_ssm_parameter" "omni_ltl_location_update_log_table_status_index" {
+#   name  = "/${var.application}/${var.env}/ltl-location-update-log-table-status-index"
+#   type  = "String"
+#   value = "Status-index"
+#   tags = {
+#     Name  = "/${var.application}/${var.env}/ltl-location-update-log-table-status-index"
+#     Application = var.application
+#     CreatedBy   = var.created_by
+#     Environment = var.env
+#     STAGE       = var.env
+#   }
+# }
 
 resource "aws_ssm_parameter" "omni_ltl_location_update_api_url" {
   name  = "/${var.application}/${var.env}/ltl-location-update/api.url"
